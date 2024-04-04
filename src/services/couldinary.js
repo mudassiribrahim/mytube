cd13042979fa79598c6a6bb27d78f55134a07224
mudassiribrahim <mudassir.comsats2018@gmail.com>
2024-04-04 13:42:04 +0500

    add file by cloudinary

--- src/services/couldinary.js
+++ src/services/couldinary.js
@@ -6,3 +6,18 @@ cloudinary.config({
     api_key: process.env.CLOUDINAR_API_KEY,
     api_secret: process.env.CLOUDINAR_API_SECRET,
 });
+
+const uploadAtCloudinary = async (filePath) => {
+    try {
+        if (!filePath) throw new Error('file path is missing ');
+        const response = await cloudinary.uploader.upload(filePath, {
+            resource_type: 'auto',
+        });
+        return response;
+    } catch (error) {
+        fs.unlinkSync(filePath);
+        return null;
+    }
+};
+
+export { uploadAtCloudinary };
