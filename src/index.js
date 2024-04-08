eeb664970ebedb551a5ce3ec54a67e800466b328
mudassiribrahim <mudassir.comsats2018@gmail.com>
2024-04-08 13:47:59 +0500

    add user router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import connectDb from './db/db-connection.js';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/users-route.js';
 dotenv.config({
     patht: './env',
 });
 
 const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
 const port = process.env.PORT || 7000;
 
 connectDb()
@@ -17,3 +28,9 @@ connectDb()
     .catch((err) => {
         console.log('Mongo db connection error :', err);
     });
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use('/api/v1/users', userRouter);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,7 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        return await fn(req, res, next);
-    } catch (error) {
-        res.status(err.code || 500).json({
-            success: false,
-            message: err.message,
-        });
-    }
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    };
 };
+
+export default asyncHandler;
