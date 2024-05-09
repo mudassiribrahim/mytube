852951e5dc7f99e2346bd8fe53f3ae3b5ed9d42a
mudassiribrahim <mudassir.comsats2018@gmail.com>
2024-05-09 10:51:03 +0500

    fix import statment in auth middelware

--- src/middlewares/auth-middelware.js
+++ src/middlewares/auth-middelware.js
@@ -1,5 +1,7 @@
 import { ApiError } from '../utils/ApiError';
 import asyncHandler from '../utils/asyncHandler';
+import jwt from 'jsonwebtoken';
+import { User } from '../schema/users';
 
 export const verifyJwt = asyncHandler(async (req, _, next) => {
     try {
